97b76f08522bab407b859e7ca0e0f974de6eb659
Melissa Vallery <mvallery88@gmail.com>
2021-05-25 22:01:38 -0500

    initial commit -general layout complete

---
+++ script.js
@@ -0,0 +1,29 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > $(window).height()) {
+        $(".header").addClass("headerActive");
+        $(".headerButton").addClass("active");
+        $(".buttonList").addClass("activeBorder");
+    } else {
+       $(".header").removeClass("headerActive");
+       $(".headerButton").removeClass("active");
+       $(".buttonList").removeClass("activeBorder");
+    }
+});
+
+$("#workButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".workSection").offset().top+2},
+        'slow');
+});
+
+$("#aboutButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".aboutSection").offset().top},
+        'slow');
+});
+
+$("#contactButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contactSection").offset().top},
+        'slow');
+});
